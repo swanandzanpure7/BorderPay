89f3c9bd260d4e60904f374efcaeb68aac2ae9bd
BorderPay Dev <dev@borderpay.app>
2026-08-22 00:46:16 +0530

    perf: disable caching on new job page

--- app/jobs/new/page.tsx
+++ app/jobs/new/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 import { useState, useCallback, useEffect } from "react";
 import { useRouter } from "next/navigation";
